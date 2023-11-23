a03aec2e6ca4132c539bf9a3f6d101cf7e190f61
0xAnon <utkarshshail005@protonmail.com>
2023-11-23 10:01:48 +0530

    Delete button

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,21 +1,20 @@
 import React, { useState } from 'react';
 import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
 import "../CSS/Home.css"
 
 function Home() {
-
   const [notes, setNotes] = useState([
     { title: 'Note Title', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam auctor eleifend velit a lobortis. Curabitur vitae porta enim. Cras scelerisque tristique diam ut placerat. Cras eleifend euismod odio at rutrum. Morbi porttitor quis augue quis egestas. Cras a elementum lectus. Integer id tempor augue, sit amet elementum nibh. Nam cursus enim ut nunc hendrerit lobortis. Aenean sed dui dictum, tempus risus non, finibus purus. Suspendisse vel nisl et ligula pulvinar aliquam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In hac habitasse platea dictumst.' },
     { title: 'Note Title', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam auctor eleifend velit a lobortis. Curabitur vitae porta enim. Cras scelerisque tristique diam ut placerat. Cras eleifend euismod odio at rutrum. Morbi porttitor quis augue quis egestas. Cras a elementum lectus. Integer id tempor augue, sit amet elementum nibh. Nam cursus enim ut nunc hendrerit lobortis. Aenean sed dui dictum, tempus risus non, finibus purus. Suspendisse vel nisl et ligula pulvinar aliquam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In hac habitasse platea dictumst.' },
     { title: 'Note Title', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam auctor eleifend velit a lobortis. Curabitur vitae porta enim. Cras scelerisque tristique diam ut placerat. Cras eleifend euismod odio at rutrum. Morbi porttitor quis augue quis egestas. Cras a elementum lectus. Integer id tempor augue, sit amet elementum nibh. Nam cursus enim ut nunc hendrerit lobortis. Aenean sed dui dictum, tempus risus non, finibus purus. Suspendisse vel nisl et ligula pulvinar aliquam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In hac habitasse platea dictumst.' },
   ]);
 
-  const [newNote, setNewNote] = useState(
-    {
-      title:"Title",
-      text:"Note"
-    }
-  )
+  const [newNote, setNewNote] = useState({
+    title: "Title",
+    text: "Note",
+  });
 
   const handleTitleChange = (index, event) => {
     const newNotes = [...notes];
@@ -33,56 +32,59 @@ function Home() {
     const newNotes = [...notes];
     newNotes.push({ ...newNote });
     setNotes(newNotes);
-  
+
     setNewNote({
       title: "Title",
       text: "Note",
     });
-  
+
     const titleInput = document.querySelector('.input-note .note-title');
     const textInput = document.querySelector('.input-note .note-text');
-  
+
     if (titleInput && textInput) {
       titleInput.innerText = "Title";
       textInput.innerText = "Note";
     }
-    
   };
+
+  const deleteNote = (index) => {
+    const updatedNotes = [...notes];
+    updatedNotes.splice(index, 1);
+    setNotes(updatedNotes);
+  };
+
   return (
     <div className="main">
-
       <div className="main-title">
         <p>My Notes</p>
       </div>
 
-      <div  className='input-note'>
-        
-            <p
-              className='note-title'
-              contentEditable
-              onBlur={(event) => {
-                newNote.title = event.target.innerText;
-                setNewNote(newNote)
-              }}
-            >
-              {newNote.title}
-            </p>
-            <p
-              className='note-text'
-              contentEditable
-              onBlur={(event) => {
-                newNote.text = event.target.innerText;
-                setNewNote(newNote)
-              }}
-            >
-              {newNote.text}
-            </p>
+      <div className='input-note'>
+        <p
+          className='note-title'
+          contentEditable
+          onBlur={(event) => {
+            newNote.title = event.target.innerText;
+            setNewNote(newNote);
+          }}
+        >
+          {newNote.title}
+        </p>
+        <p
+          className='note-text'
+          contentEditable
+          onBlur={(event) => {
+            newNote.text = event.target.innerText;
+            setNewNote(newNote);
+          }}
+        >
+          {newNote.text}
+        </p>
 
-            <Button variant="contained" size="small"  onClick={addNewNote}>
-            New Note
-            </Button>
-        
-        </div>
+        <Button variant="contained" size="small" onClick={addNewNote}>
+          New Note
+        </Button>
+      </div>
 
       <div className='allNotes'>
         {notes.map((note, index) => (
@@ -101,10 +103,14 @@ function Home() {
             >
               {note.text}
             </p>
+            <span className='deleteIcon'>
+              <IconButton color="primary" onClick={() => deleteNote(index)}>
+                <DeleteIcon fontSize="inherit" />
+              </IconButton>
+            </span>
           </div>
         ))}
       </div>
-
     </div>
   );
 }
