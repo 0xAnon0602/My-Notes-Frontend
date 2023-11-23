be0deb32f5c39d5a5678703e75fb64b7f65f2486
0xAnon <utkarshshail005@protonmail.com>
2023-11-23 06:22:21 +0530

    Basic homepage

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,8 +1,44 @@
+import Button from '@mui/material/Button';
+import "../CSS/Home.css"
 
 function Home() {
     return (
-      <div >
-        Testing
+      <div className="main">
+
+        <div className="main-title">
+            <p>My Notes</p>
+        </div>
+        
+        {/* <Button className='google-button' variant="contained">
+          Sign in with Google
+        </Button> */}
+
+        <div className='allNotes'>
+
+          <div className='note'>
+
+            <p className='note-title'>Note Title</p>
+            <p className='note-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam auctor eleifend velit a lobortis. Curabitur vitae porta enim. Cras scelerisque tristique diam ut placerat. Cras eleifend euismod odio at rutrum. Morbi porttitor quis augue quis egestas. Cras a elementum lectus. Integer id tempor augue, sit amet elementum nibh. Nam cursus enim ut nunc hendrerit lobortis. Aenean sed dui dictum, tempus risus non, finibus purus. Suspendisse vel nisl et ligula pulvinar aliquam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In hac habitasse platea dictumst.</p>
+
+          </div>
+
+          <div className='note'>
+
+            <p className='note-title'>Note Title</p>
+            <p className='note-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam auctor eleifend velit a lobortis. Curabitur vitae porta enim. Cras scelerisque tristique diam ut placerat. Cras eleifend euismod odio at rutrum. Morbi porttitor quis augue quis egestas. Cras a elementum lectus. Integer id tempor augue, sit amet elementum nibh. Nam cursus enim ut nunc hendrerit lobortis. Aenean sed dui dictum, tempus risus non, finibus purus. Suspendisse vel nisl et ligula pulvinar aliquam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In hac habitasse platea dictumst.</p>
+
+          </div>
+          
+          <div className='note'>
+
+            <p className='note-title'>Note Title</p>
+            <p className='note-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam auctor eleifend velit a lobortis. Curabitur vitae porta enim. Cras scelerisque tristique diam ut placerat. Cras eleifend euismod odio at rutrum. Morbi porttitor quis augue quis egestas. Cras a elementum lectus. Integer id tempor augue, sit amet elementum nibh. Nam cursus enim ut nunc hendrerit lobortis. Aenean sed dui dictum, tempus risus non, finibus purus. Suspendisse vel nisl et ligula pulvinar aliquam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In hac habitasse platea dictumst.</p>
+
+          </div>
+          
+
+        </div>
+      
       </div>
     );
   }
