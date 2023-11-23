7041da57be483605e2d37a09057d2295b93cf71c
0xAnon <utkarshshail005@protonmail.com>
2023-11-23 07:05:39 +0530

    New note section added

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,47 +1,99 @@
+import React, { useState } from 'react';
 import Button from '@mui/material/Button';
 import "../CSS/Home.css"
 
 function Home() {
-    return (
-      <div className="main">
 
-        <div className="main-title">
-            <p>My Notes</p>
-        </div>
-        
-        {/* <Button className='google-button' variant="contained">
-          Sign in with Google
-        </Button> */}
+  const [notes, setNotes] = useState([
+    { title: 'Note Title', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam auctor eleifend velit a lobortis. Curabitur vitae porta enim. Cras scelerisque tristique diam ut placerat. Cras eleifend euismod odio at rutrum. Morbi porttitor quis augue quis egestas. Cras a elementum lectus. Integer id tempor augue, sit amet elementum nibh. Nam cursus enim ut nunc hendrerit lobortis. Aenean sed dui dictum, tempus risus non, finibus purus. Suspendisse vel nisl et ligula pulvinar aliquam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In hac habitasse platea dictumst.' },
+    { title: 'Note Title', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam auctor eleifend velit a lobortis. Curabitur vitae porta enim. Cras scelerisque tristique diam ut placerat. Cras eleifend euismod odio at rutrum. Morbi porttitor quis augue quis egestas. Cras a elementum lectus. Integer id tempor augue, sit amet elementum nibh. Nam cursus enim ut nunc hendrerit lobortis. Aenean sed dui dictum, tempus risus non, finibus purus. Suspendisse vel nisl et ligula pulvinar aliquam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In hac habitasse platea dictumst.' },
+    { title: 'Note Title', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam auctor eleifend velit a lobortis. Curabitur vitae porta enim. Cras scelerisque tristique diam ut placerat. Cras eleifend euismod odio at rutrum. Morbi porttitor quis augue quis egestas. Cras a elementum lectus. Integer id tempor augue, sit amet elementum nibh. Nam cursus enim ut nunc hendrerit lobortis. Aenean sed dui dictum, tempus risus non, finibus purus. Suspendisse vel nisl et ligula pulvinar aliquam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In hac habitasse platea dictumst.' },
+  ]);
 
-        <div className='allNotes'>
+  const [newNote, setNewNote] = useState(
+    {
+      title:"Title",
+      text:"Note"
+    }
+  )
 
-          <div className='note'>
+  const handleTitleChange = (index, event) => {
+    const newNotes = [...notes];
+    newNotes[index].title = event.target.innerText;
+    setNotes(newNotes);
+  };
 
-            <p className='note-title'>Note Title</p>
-            <p className='note-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam auctor eleifend velit a lobortis. Curabitur vitae porta enim. Cras scelerisque tristique diam ut placerat. Cras eleifend euismod odio at rutrum. Morbi porttitor quis augue quis egestas. Cras a elementum lectus. Integer id tempor augue, sit amet elementum nibh. Nam cursus enim ut nunc hendrerit lobortis. Aenean sed dui dictum, tempus risus non, finibus purus. Suspendisse vel nisl et ligula pulvinar aliquam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In hac habitasse platea dictumst.</p>
+  const handleTextChange = (index, event) => {
+    const newNotes = [...notes];
+    newNotes[index].text = event.target.innerText;
+    setNotes(newNotes);
+  };
 
-          </div>
-
-          <div className='note'>
-
-            <p className='note-title'>Note Title</p>
-            <p className='note-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam auctor eleifend velit a lobortis. Curabitur vitae porta enim. Cras scelerisque tristique diam ut placerat. Cras eleifend euismod odio at rutrum. Morbi porttitor quis augue quis egestas. Cras a elementum lectus. Integer id tempor augue, sit amet elementum nibh. Nam cursus enim ut nunc hendrerit lobortis. Aenean sed dui dictum, tempus risus non, finibus purus. Suspendisse vel nisl et ligula pulvinar aliquam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In hac habitasse platea dictumst.</p>
-
-          </div>
-          
-          <div className='note'>
-
-            <p className='note-title'>Note Title</p>
-            <p className='note-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam auctor eleifend velit a lobortis. Curabitur vitae porta enim. Cras scelerisque tristique diam ut placerat. Cras eleifend euismod odio at rutrum. Morbi porttitor quis augue quis egestas. Cras a elementum lectus. Integer id tempor augue, sit amet elementum nibh. Nam cursus enim ut nunc hendrerit lobortis. Aenean sed dui dictum, tempus risus non, finibus purus. Suspendisse vel nisl et ligula pulvinar aliquam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In hac habitasse platea dictumst.</p>
-
-          </div>
-          
-
-        </div>
-      
-      </div>
-    );
+  const addNewNote = () => {
+    const newNotes = [...notes];
+    newNotes.push(newNote)
+    setNotes(newNotes);
   }
-  
-  export default Home;
-  
+
+  return (
+    <div className="main">
+
+      <div className="main-title">
+        <p>My Notes</p>
+      </div>
+
+      <div  className='input-note'>
+        
+            <p
+              className='note-title'
+              contentEditable
+              onBlur={(event) => {
+                newNote.title = event.target.innerText;
+                setNewNote(newNote)
+              }}
+            >
+              {newNote.title}
+            </p>
+            <p
+              className='note-text'
+              contentEditable
+              onBlur={(event) => {
+                newNote.text = event.target.innerText;
+                setNewNote(newNote)
+              }}
+            >
+              {newNote.text}
+            </p>
+
+            <Button variant="contained" size="small"  onClick={addNewNote}>
+            New Note
+            </Button>
+        
+        </div>
+
+      <div className='allNotes'>
+        {notes.map((note, index) => (
+          <div key={index} className='note'>
+            <p
+              className='note-title'
+              contentEditable
+              onBlur={(event) => handleTitleChange(index, event)}
+            >
+              {note.title}
+            </p>
+            <p
+              className='note-text'
+              contentEditable
+              onBlur={(event) => handleTextChange(index, event)}
+            >
+              {note.text}
+            </p>
+          </div>
+        ))}
+      </div>
+
+    </div>
+  );
+}
+
+export default Home;
